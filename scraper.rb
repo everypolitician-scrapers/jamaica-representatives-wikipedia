b68927d293c768fd64427764d1c0da23cb7bd97c
Tony Bowden <tony@tmtm.com>
2018-10-03 11:54:18 +0100

    Gather data, and save it in one shot

--- scraper.rb
+++ scraper.rb
@@ -26,7 +26,7 @@ def scrape_list(term, url)
   noko = noko_for(url)
 
   current_parish = ''
-  noko.xpath('//h2[contains(span,"Constituencies and MPs")]/following-sibling::table[1]//tr[td]').each do |tr|
+  noko.xpath('//h2[contains(span,"Constituencies and MPs")]/following-sibling::table[1]//tr[td]').map do |tr|
     tds = tr.css('td')
 
     unless (parish = tds[0].css('a').text).empty?
@@ -35,7 +35,7 @@ def scrape_list(term, url)
     constituency = tds[1].css('a').text
     area_id = 'ocd-division/country:ja/parish:%s/constituency:%s' % [ocd_idify(current_parish), ocd_idify(constituency)]
 
-    data = {
+    {
       name:     tds[2].text,
       wikiname: tds[2].xpath('.//a[not(@class="new")]/@title').text,
       party:    tds[3].text.tidy,
@@ -45,11 +45,13 @@ def scrape_list(term, url)
       term:     term,
       source:   url,
     }
-    puts data.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h if ENV['MORPH_DEBUG']
-    ScraperWiki.save_sqlite(%i(name party term), data)
   end
 end
 
+data_2011 = scrape_list(2011, 'https://en.wikipedia.org/w/index.php?title=Constituencies_of_Jamaica&oldid=707616860')
+data_2016 = scrape_list(2016, 'https://en.wikipedia.org/wiki/Constituencies_of_Jamaica')
+data = data_2011 + data_2016
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-scrape_list(2011, 'https://en.wikipedia.org/w/index.php?title=Constituencies_of_Jamaica&oldid=707616860')
-scrape_list(2016, 'https://en.wikipedia.org/wiki/Constituencies_of_Jamaica')
+ScraperWiki.save_sqlite(%i(name party term), data)
